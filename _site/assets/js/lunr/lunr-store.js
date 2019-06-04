11ed876dabe8586ccee1795c16b62b1015c7ccb1
Ani Kembhavi <anik@allenai.org>
2019-06-04 12:13:36 -0700

    New website first commit

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Vanilla VQA",
+        "excerpt":"Visual Question Answering (VQA) is the task of answering questions about a given piece of visual content such as an image, video or infographic. As seen in the examples below, answering questions about visual content requires a variety of skills include recognizing entities and objects, reasoning about their interactions with...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/vanilla-vqa/",
+        "teaser":null}]
